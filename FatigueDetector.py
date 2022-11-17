1654a6e7f3f44c420ec373d7ca59c55c9f2a7604
dvirbens <dvirbens@gmail.com>
2022-11-17 18:04:13 +0200

    Hey baby its blik didect method

--- FatigueDetector.py
+++ FatigueDetector.py
@@ -1,14 +1,50 @@
+from scipy.spatial import distance
+
 
 class FatigueDetector:
     blinks_per_minuets = 0
     numbers_of_yaws = 0
     number_of_snooze = 0
+    ear_threshold = 0.2
+    closed_eye = False
+    open_eye = False
 
-    def blink_detection(self, left_eye, right_eye):
+    def blink_detection(self, left_eye: list, right_eye: list):
         """
 
         :return:
         """
+        left_ear = self._eye_aspect_ratio_calculate(left_eye)
+        right_ear = self._eye_aspect_ratio_calculate(right_eye)
+        avg_ear = (left_ear + right_ear) / 2
+        if avg_ear <= self.ear_threshold:
+            self.closed_eye = True
+        if avg_ear > self.ear_threshold + 0.05:
+            self.open_eye = True
+        if self.closed_eye and self.open_eye:
+            self.closed_eye = False
+            self.open_eye = False
+            self.blinks_per_minuets = self.blinks_per_minuets + 1
+
+        return avg_ear
+
+    def _eye_aspect_ratio_calculate(self, eye: list):
+        """
+
+        :param eye:
+        :return:
+        """
+        p1 = [eye[0].x, eye[0].y]
+        p2 = [eye[1].x, eye[1].y]
+        p3 = [eye[2].x, eye[2].y]
+        p4 = [eye[3].x, eye[3].y]
+        p5 = [eye[4].x, eye[4].y]
+        p6 = [eye[5].x, eye[5].y]
+        AL = distance.euclidean(p2, p6)
+        BL = distance.euclidean(p3, p5)
+        CL = distance.euclidean(p1, p4)
+        EAR = (AL + BL) / (2 * CL)
+        return EAR
 
     def yawning_detection(self, mouth):
         """
--- StayAwake.py
+++ StayAwake.py
@@ -1,5 +1,7 @@
 import cv2
 import dlib
+import matplotlib.pyplot as plt
+import time
 
 from FatigueDetector import FatigueDetector
 from SleepDetector import SleepDetector
@@ -14,7 +16,16 @@ class StayAwake:
     cap = cv2.VideoCapture(0)
 
     def run(self):
+        frame_cnt = 0
+        EAR = [0]
+        prev_frame_time = 0
         while True:
+            new_frame_time = time.time()
+            fps = 1 / (new_frame_time - prev_frame_time)
+            prev_frame_time = new_frame_time
+            fps = int(fps)
+            fps = str(fps)
+            frame_cnt = frame_cnt + 1
             # Find haar cascade to draw bounding box around face
             ret, frame = self.cap.read()
             if not ret:
@@ -30,17 +41,49 @@ class StayAwake:
                     y2 = face.bottom()
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     landmarks = self.predictor(gray, face)
-                    for i in range(0, 67):
+                    left_eye = []
+                    right_eye = []
+
+                    for i in range(6):
+                        left_eye.append(landmarks.part(36 + i))
+                        right_eye.append(landmarks.part(42 + i))
+
+                    for i in range(36, 48):
                         x = landmarks.part(i).x
                         y = landmarks.part(i).y
                         cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
+                    EAR.append(self.fatigue_detector.blink_detection(left_eye, right_eye))
+                    cv2.putText(frame, f"EAR:{EAR[-1]} ", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                    cv2.putText(frame, f"fps:{fps} ", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                    cv2.putText(frame, f"Blinks:{self.fatigue_detector.blinks_per_minuets} ", (50, 50),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
             cv2.imshow("Frame", frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+        print(EAR)
+        close_eye = False
+        blink = 0
+        for i in range(0, len(EAR) - 1):
+            if EAR[i] < 0.2:
+                if EAR[i + 1] > 0.2 or EAR[i + 2] > 0.2 or EAR[i + 3] > 0.2:
+                    blink = blink + 1
+
+        plt.plot(EAR)
+        print(blink)
+        # naming the x axis
+        plt.xlabel('x - axis')
+        # naming the y axis
+        plt.ylabel('y - axis')
+
+        # function to show the plot
+        plt.show()
         self.cap.release()
+        new_frame_time = time.time()
+
+        # Calculating the fps
+
         cv2.destroyAllWindows()
-        print("dvir")
-        #dd
